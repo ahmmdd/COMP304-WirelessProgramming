e159b29bb1464a5d2096209d1b035a4f21f5fd0d
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-03-23 13:14:50 -0400

    Initial commit

---
+++ app/src/main/java/com/cc/mja/intentusesamplelab1/Constants.java
@@ -0,0 +1,11 @@
+package com.cc.mja.intentusesamplelab1;
+
+/**
+ * Created by T875341 on 17/09/2016.
+ */
+public class Constants {
+    public static String key_first_name = "key_first_name";
+    public static String key_last_name = "key_last_name";
+
+    public static String key_student = "key_student";
+}
