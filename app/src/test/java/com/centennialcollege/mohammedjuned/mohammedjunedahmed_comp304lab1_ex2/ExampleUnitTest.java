780f2f8a786bda3868f8c2c2760c1fd03a49bd36
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-03-23 14:19:43 -0400

    A1E2

--- app/src/test/java/com/centennialcollege/mohammedjuned/mohammedjunedahmed_comp304lab1_ex2/ExampleUnitTest.java
+++ app/src/test/java/com/centennialcollege/mohammedjuned/mohammedjunedahmed_comp304lab1_ex2/ExampleUnitTest.java
@@ -1,13 +1,11 @@
-package com.mja.lab7databasessqlite;
+package com.centennialcollege.mohammedjuned.mohammedjunedahmed_comp304lab1_ex2;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
